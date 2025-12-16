3ce0d61aba9644122c1103835e6ef36e22cb3c21
SemihAslan123 <semihaslan2210@gmail.com>
2025-12-16 13:13:24 +0100

    first commit

---
+++ database/migrations/2025_12_16_104519_create_editor_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('editor_requests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->enum('status', ['pending', 'approved', 'rejected']);
+            $table->longText('admin_comment')->nullable();
+            $table->timestamp('processed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('editor_requests');
+    }
+};
