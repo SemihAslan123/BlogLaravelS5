ce761e74d7f482a0604ce01c2a3ef065f7801252
GANABA Melchisedech <ganabamelchisedech@gmail.com>
2025-12-26 23:38:08 -0800

    Création des controller et des Gates

---
+++ app/Http/Controllers/EditorRequestController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EditorRequestController extends Controller
+{
+    //
+    public function index(){
+        //
+    }
+
+    public function store(Request $request){
+        //
+    }
+
+    public function approve($id){
+        //
+    }
+
+    public function reject(Request $request, $id){
+        //
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,40 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Gate pour vérifier si l'utilisateur est admin
+        Gate::define('is-admin', function (User $user) {
+            return $user->role === 'admin';
+        });
+
+        // Gate pour vérifier si l'utilisateur est éditeur
+        Gate::define('is-editor', function (User $user) {
+            return $user->role === 'editor';
+        });
+
+        // Gate pour vérifier si l'utilisateur peut créer des articles
+        Gate::define('create-article', function (User $user) {
+            return $user->role === 'editor';
+        });
+
+        // Gate pour vérifier si l'utilisateur peut modifier un article
+        Gate::define('update-article', function (User $user, Article $article) {
+            return $user->id === $article->user_id && $user->role === 'editor';
+        });
+
+        // Gate pour vérifier si l'utilisateur peut supprimer un article
+        Gate::define('delete-article', function (User $user, Article $article) {
+            return $user->id === $article->user_id && $user->role === 'editor';
+        });
+
+        // Gate pour modifier un commentaire
+        Gate::define('update-comment', function (User $user, Comment $comment) {
+            return $user->id === $comment->user_id;
+        });
+
+        // Gate pour supprimer un commentaire
+        Gate::define('delete-comment', function (User $user, Comment $comment) {
+            return $user->id === $comment->user_id;
+        });
+
     }
 }
